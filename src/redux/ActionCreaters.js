1507eb10539e1b28f2bdf442945b6813886b439a
Arihant416 <arihantjain416@gmail.com>
2020-05-09 16:33:25 +0530

    Issue in Redux Thunk

--- src/redux/ActionCreaters.js
+++ src/redux/ActionCreaters.js
@@ -1,4 +1,5 @@
 import * as ActionTypes from './ActionTypes';
+import { DISHES } from '../shared/Dishes';
 
 export const addComment = (dishId, rating, author, comment) => ({
     type: ActionTypes.ADD_COMMENT,
@@ -8,4 +9,26 @@ export const addComment = (dishId, rating, author, comment) => ({
         comment: comment,
         author: author,
     },
+});
+
+export const fetchDishes = () => (dispatch) => {
+    dispatch(dishesLoading(true));
+
+    setTimeout(() => {
+        dispatch(addDishes(DISHES));
+    }, 2);
+};
+
+export const dishesLoading = () => (dispatch) => ({
+    type: ActionTypes.DISHES_LOADING,
+});
+
+export const dishesFailed = (errmess) => ({
+    type: ActionTypes.DISHES_FAILED,
+    payload: errmess,
+});
+
+export const addDishes = (dishes) => ({
+    type: ActionTypes.ADD_DISHES,
+    payload: dishes,
 });
